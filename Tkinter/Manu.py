4222ff15f26950560210f8a011a1c297632e9f50
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2023-01-06 16:34:27 +0530

    Upload

--- Tkinter/Manu.py
+++ Tkinter/Manu.py
@@ -2,7 +2,7 @@ from tkinter import *
 root = Tk()
 menubar= Menu(root)
 filemenu= Menu(menubar, tearoff=0)
-filemenu.add_command(label="New")
+filemenu.add_command(label="New" )
 filemenu.add_command(label="Open")
 filemenu.add_command(label="Save")
 filemenu.add_command(label="Close")
